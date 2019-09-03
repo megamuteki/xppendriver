cdf9156728765ceef4385f60c5a7410cc2d90642
Masaru Hirano <mhirano@neoblast.co.jp>
2019-09-03 15:51:42 +0900

    Add files via upload

--- UNINSTALL.sh
+++ UNINSTALL.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
-dir1=/opt/pentablet/xppen; [  -e $dir ] && sudo  rm  -rRf  $dir1
-dir2=/opt/pentablet/*; [ ! -e $dir ] && sudo  rm  -rRf  $dir2
-dir3=/usr/share/pixmaps/pixmaps; [  -e $dir ] && sudo  rm  -rRf  $dir3
+
+#desktop files
 file1=/usr/share/pixmaps/xppen.svg; [  -e $dir ] && sudo  rm  $file1
 file2=/usr/share/pixmaps/xppen.xpm; [  -e $dir ] && sudo  rm  $file2
 file3=/usr/share/applications/xppentablet.desktop; [  -e $dir ] && sudo  rm  $file3
+
+#xppen directory
+dir1=/opt/pentablet/xppen
+if [ -n "$(ls $dir1)" ]; then
+    sudo rm -r $dir1
+fi
+
+#pentablet dir
+dir2=/opt/pentablet/
+if [ -z "$(ls $dir2)" ]; then
+    sudo rm -r $dir2
+fi
+
+
+
 exit
